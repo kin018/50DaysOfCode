2b25c55fb5b16800015245c46ef836540541202c
kin018 <60412598+kin018@users.noreply.github.com>
2023-10-27 16:03:22 -0400

    clean up

--- reactProgressSteps/script.js
+++ reactProgressSteps/script.js
@@ -1,43 +1,57 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
-const stepDescription = document.getElementById('step-description');
+// Retrieving DOM elements
+const progress = document.getElementById('progress'); // The progress bar
+const prev = document.getElementById('prev'); // Button to go to previous step
+const next = document.getElementById('next'); // Button to go to next step
+const circles = document.querySelectorAll('.circle'); // All step circles
+const stepDescription = document.getElementById('step-description'); // Element to display step description
 
+// Total number of steps
 const totalSteps = circles.length;
+
+// Current active step
 let currActive = 1;
 
+// Event listener for the "Next" button
 next.addEventListener('click', () => {
   if (currActive < totalSteps) {
-    currActive++;
-    update();
+    currActive++; // Move to the next step
+    update(); // Update the UI
   }
 });
 
+// Event listener for the "Previous" button
 prev.addEventListener('click', () => {
   if (currActive > 1) {
-    currActive--;
-    update();
+    currActive--; // Move to the previous step
+    update(); // Update the UI
   }
 });
 
+// Function to update the UI based on the current active step
 function update() {
+  // Loop through all circles to update their active/inactive status
   circles.forEach((circle, index) => {
     if (index < currActive) {
-      circle.classList.add('active');
+      circle.classList.add('active'); // Mark circles up to the active step as active
     } else {
-      circle.classList.remove('active');
+      circle.classList.remove('active'); // Mark circles after the active step as inactive
     }
   });
 
-  const actives = document.querySelectorAll('.active');
+  // Calculate the width of the progress bar based on the current active step
   const width = ((currActive - 1) / (totalSteps - 1)) * 100 + '%';
 
+  // Set the width of the progress bar
   progress.style.width = width;
+
+  // Disable the buttons at the start and end of steps
   prev.disabled = currActive === 1;
   next.disabled = currActive === totalSteps;
+
+  // Set the step description based on the current active step
   stepDescription.textContent =
     circles[currActive - 1].getAttribute('step-step');
 }
 
+// Initially update the UI for the first step
 update();
